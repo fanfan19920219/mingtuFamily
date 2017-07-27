80757d7ec73f4276ab6c26eb866cb5b85f5c1f79
406608969@qq.com <superhero@StardeMacBook-Pro.local>
2017-07-28 00:36:18 +0800

    zzh________________________________

---
+++ MingTu/businessViewController.h
@@ -0,0 +1,13 @@
+//
+//  businessViewController.h
+//  MingTu
+//
+//  Created by Star J on 2017/7/28.
+//  Copyright © 2017年 zhangzhihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface businessViewController : UIViewController
+
+@end
---
+++ MingTu/businessViewController.m
@@ -0,0 +1,37 @@
+//
+//  businessViewController.m
+//  MingTu
+//
+//  Created by Star J on 2017/7/28.
+//  Copyright © 2017年 zhangzhihua. All rights reserved.
+//
+
+#import "businessViewController.h"
+
+@interface businessViewController ()
+
+@end
+
+@implementation businessViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
